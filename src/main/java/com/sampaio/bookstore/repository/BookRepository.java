cde017b2e5f01e1f973501548e9047c27bfc891b
carlosampaio <carlos.a.m.sampaio@gmail.com>
2021-05-03 08:03:00 -0300

    Terceiro commit: Inclusao das entidades Autor e Book

---
+++ src/main/java/com/sampaio/bookstore/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.sampaio.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sampaio.bookstore.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
