7b4f795f2eb8c7ed7a8239ad1bcc6c3172792f6b
carlosampaio <carlos.a.m.sampaio@gmail.com>
2021-05-03 06:23:03 -0300

    Primeiro commit: Criacao da estrutura do projeto

---
+++ src/test/java/com/sampaio/bookstore/StorebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampaio.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
